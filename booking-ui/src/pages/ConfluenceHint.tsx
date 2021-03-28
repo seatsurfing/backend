26cb7554ee2ab447fa3213ae1e7831a984b406cf
virtualzone <13085858+virtualzone@users.noreply.github.com>
2021-03-28 10:53:57 +0200

    Improved Confluence hint.

--- booking-ui/src/pages/ConfluenceHint.tsx
+++ booking-ui/src/pages/ConfluenceHint.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import './Login.css';
-import { Form, Alert, Button } from 'react-bootstrap';
+import './ConfluenceHint.css';
+import { Button } from 'react-bootstrap';
 import { RouteChildrenProps } from 'react-router-dom';
+import { Copy as IconCopy } from 'react-feather';
 import { withTranslation } from 'react-i18next';
 import { TFunction } from 'i18next';
 
@@ -17,20 +18,56 @@ interface Props extends RouteChildrenProps<RoutedProps> {
 }
 
 class ConfluenceHint extends React.Component<Props, State> {
-  onCreateAccountClick = () => {
-    window.open("https://seatsurfing.de/");
+  onCreateAccountClick = (e: any) => {
+    e.preventDefault();
+    window.open("https://seatsurfing.de/sign-up/");
+  }
+
+  onAdminClick = (e: any) => {
+    e.preventDefault();
+    window.open("https://app.seatsurfing.de/admin/");
+  }
+
+  onHelpClick = (e: any) => {
+    e.preventDefault();
+    window.open("https://seatsurfing.de/contact/");
+  }
+
+  onInputClick = (e: any) => {
+    e.preventDefault();
+    let input = document.querySelector("input.copy-input");
+    if (input) {
+      (input as HTMLInputElement).select();
+    }
+  }
+
+  onCopyClick = () => {
+    let input = document.querySelector("input.copy-input");
+    if (input) {
+      (input as HTMLInputElement).select();
+      document.execCommand("copy");
+    }
   }
 
   render() {
     return (
-      <div className="container-signin">
-        <Form className="form-signin">
-          <Alert variant="danger">{this.props.t("errorConfluenceClientIdUnknown")}</Alert>
+      <div className="container-confluence">
+          <h1>{this.props.t("errorConfluenceClientIdUnknown")}</h1>
           <p>{this.props.t("confluenceClientIdHint")}</p>
-          <pre>{this.props.match?.params.id}</pre>
-          <p>{this.props.t("confluenceClientIdHint2")}</p>
-          <Button className="btn btn-primary" onClick={this.onCreateAccountClick}>{this.props.t("createAccount")}</Button>
-        </Form>
+          <ol>
+            <li><a href="#" onClick={this.onCreateAccountClick}>{this.props.t("confluenceClientIdStep1")}</a></li>
+            <li><a href="#" onClick={this.onAdminClick}>{this.props.t("confluenceClientIdStep2")}</a></li>
+            <li>{this.props.t("confluenceClientIdStep3")}</li>
+            <li>
+              {this.props.t("confluenceClientIdStep4")}
+              <br />
+              <input type="text" className="copy-input" size={36} onClick={this.onInputClick} value={this.props.match?.params.id} readOnly={true} />
+              <Button variant="link" size="sm" className="copy-button" onClick={this.onCopyClick}><IconCopy className="feather" /></Button>
+            </li>
+            <li>{this.props.t("confluenceClientIdStep5")}</li>
+            <li>{this.props.t("confluenceClientIdStep6")}</li>
+          </ol>
+          <p>{this.props.t("confluenceClientIdHint2")} <a href="#" onClick={this.onHelpClick}>{this.props.t("confluenceClientIdHint3")}</a></p>
       </div>
     )
   }
