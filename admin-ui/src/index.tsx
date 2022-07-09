5e8df2b0f3f7483f7c13aa543e8d6fc81beee6d1
virtualzone <13085858+virtualzone@users.noreply.github.com>
2022-07-09 22:31:47 +0200

    Upgrade to React v18

--- admin-ui/src/index.tsx
+++ admin-ui/src/index.tsx
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App tab="home" />);
