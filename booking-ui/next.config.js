72d4e90dfdc54c2aa9f59841a3af76a86c9282ee
virtualzone <13085858+virtualzone@users.noreply.github.com>
2023-04-17 22:38:21 +0200

    Optimized booking ui docker build

--- booking-ui/next.config.js
+++ booking-ui/next.config.js
@@ -3,6 +3,7 @@ const { i18n } = require('./next-i18next.config')
 const nextConfig = {
   i18n,
   distDir: 'build',
+  output: 'standalone',
   reactStrictMode: true,
   basePath: '/ui',
   async redirects() {
