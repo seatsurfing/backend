d55f6daec5b73363ec64d768ae2e81078087e13e
Folke Ashberg <folke@ashberg.de>
2023-06-23 23:57:54 +0200

    readded colon

--- admin-ui/next-i18next.config.js
+++ admin-ui/next-i18next.config.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   i18n: {
     defaultLocale: 'default',
-    locales: ['default', 'en', 'de', 'fr']
+    locales: ['default', 'en', 'de', 'fr'],
   },
   serializeConfig: false,
   use: isBrowser ? [ChainedBackend] : [],
